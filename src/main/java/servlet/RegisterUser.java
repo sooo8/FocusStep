768295ec83c1143cfe26d6c3b4a74434ca85d5cf
Serika OE <inu.meong.11@gmail.com>
2025-10-27 11:46:38 +0900

    初回コミット

---
+++ src/main/java/servlet/RegisterUser.java
@@ -0,0 +1,95 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import model.RegisterUserLogic;
+import model.User;
+
+/**
+ * Servlet implementation class RegisterUser
+ */
+@WebServlet("/RegisterUser")
+public class RegisterUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterUser() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		//フォワード先
+		String forwardPath = null;
+		
+		//サーブレットクラスの動作を決定する「action」の値を
+		//リクエストパラメーターから取得
+		String action = request.getParameter("action");
+		
+		//登録の開始をリクエストされた時の処理
+		if(action == null) {
+			//フォワード先を設定
+			forwardPath = "WEB-INF/jsp/registerForm.jsp";
+		}
+		else if (action.equals("done")) {
+			//セッションスコープに保存された登録ユーザーを取得
+			HttpSession session = request.getSession();
+			User registerUser = (User)session.getAttribute("registerUser");
+			
+			//登録処理の呼び出し
+			RegisterUserLogic logic = new RegisterUserLogic();
+			logic.execute(registerUser);
+			
+			//不要になったセッションスコープ内のインスタンスを削除
+			session.removeAttribute("registerUser");
+			
+			//登録後のフォワード先を設定
+			forwardPath ="WEB-INF/jsp/registerConfirm.jsp";
+		}
+		
+			//設定されたフォワード先にフォワード
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher(forwardPath);
+			dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		//リクエストパラメーターの取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("id");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		
+		//登録するユーザーの情報を設定
+		User registerUser = new User(id,name,pass);
+		
+		//セッションスコープに登録ユーザーを保存
+		HttpSession session = request.getSession();
+		session.setAttribute("registerUser", registerUser);
+		
+		//フォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/registerConfirm.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
